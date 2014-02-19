62368335465306b9a91bf1a2342fa520d4af1d89
MaryG <mgouseti@gmail.com>
2014-02-19 11:12:59 +0100

    Added DONOTHING mode and renamed mode setters

--- QLNEW/src/ql_obj_alg/operation/typechecker/tools/DependencyCycleDetection.java
+++ QLNEW/src/ql_obj_alg/operation/typechecker/tools/DependencyCycleDetection.java
@@ -12,23 +12,23 @@ public class DependencyCycleDetection {
 	DependencyGraph dependecyG = new DependencyGraph();
 	
 	private enum Mode {
-		INDEPENDENT, DEPENDONTHIS, THISDEPENDON;
+		INDEPENDENT, DEPENDONTHIS, THISDEPENDON, DONOTHING;
 	}
 
 	public DependencyCycleDetection() {
 		mode = Mode.INDEPENDENT;
 	}
 	
-	public void addNewDependecies(){
+	public void setModeNewNodesToDependendOn(){
 		mode = Mode.DEPENDONTHIS;
 		currentDependencies.push(new HashSet<String>());
 	}
 	
-	public void addNewDependentNodes(){
+	public void setModeNewDependentNodes(){
 		mode = Mode.THISDEPENDON;
 	}
 	
-	public void addIndependentNodes(){
+	public void setModeNewIndependentNodes(){
 		mode = Mode.INDEPENDENT;
 	}
 	
