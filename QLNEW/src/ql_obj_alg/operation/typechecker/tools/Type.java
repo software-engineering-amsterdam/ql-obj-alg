6f6d1072baeaefe52737af29ee9375de4064f8f4
Mary Gouseti <mgouseti@gmail.com>
2014-02-19 12:02:42 +0100

    Change Type to abstract class with basic methods for types

--- QLNEW/src/ql_obj_alg/operation/typechecker/tools/Type.java
+++ QLNEW/src/ql_obj_alg/operation/typechecker/tools/Type.java
@@ -1,96 +1,21 @@
 package ql_obj_alg.operation.typechecker.tools;
 
-public class Type {
+public abstract class Type {
 	String type;
 	
 	public Type(String type){
 		this.type = type;
 	}
 	
-	public boolean comparable(Type t){
-		if(!this.isOrd())
-			return false;
-		
-		if(t == null || !t.comparable(t))
-			return false;
-		
-		if(this.equals(t))
-			return true;
-		
-		return false;
-	}
+	public abstract boolean comparable(Type t);
 	
-	public boolean isOrd(){
-		if(type == null)
-			return false;
-		
-		if(type.equals("integer"))
-			return true;
-		
-		if(type.equals("string"))
-			return true;
-		
-		return false;
-	}
+	public abstract boolean isOrd();
 	
-	@Override
-	public boolean equals(Object obj){
-		if(obj == null)
-			return false;
-		
-		if(getClass() != obj.getClass())
-			return false;
-		
-		Type t = (Type) obj;
-		
-		if((this.type != null && this.type.equals(t.type)) || this.type == t.type )
-			return true;
-		return false;
-	}
+	public abstract boolean isNumber();
 	
-	@Override 
-	public int hashCode(){
-		return this.type.hashCode();
-	}
+	public abstract boolean isAlphanumeric();
 	
-	public boolean isInteger(){
-		if(type == null)
-			return false;
-		
-		if(type.equals("integer"))
-			return true;
-		
-		return false;
-	}
+	public abstract boolean isDate();
 	
-	public boolean isBoolean(){
-		if(type == null)
-			return false;
-		
-		if(type.equals("boolean"))
-			return true;
-		
-		return false;
-	}
-	
-	public boolean isString(){
-		if(type == null)
-			return false;
-		
-		if(type.equals("string"))
-			return true;
-		
-		return false;
-	}
-	
-	public boolean isUndefined(){
-		if(type == null)
-			return true;
-		
-		return false;
-	}
-	
-	public String toString(){
-		return this.type;
-	}
+	public abstract boolean isUndefined();
 }
