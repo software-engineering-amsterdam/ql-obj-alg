7b499a803f31611e640880e1c0ae2453204865b1
chielpeters <chielio007@gmail.com>
2014-02-20 16:05:12 +0100

    New files

---
+++ QLNEW/src/ql_obj_alg/errors/error_reporting/BailErrorStrategy.java
@@ -0,0 +1,20 @@
+package ql_obj_alg.errors.error_reporting;
+
+import org.antlr.v4.runtime.*;
+
+public class BailErrorStrategy extends DefaultErrorStrategy {
+    /** Instead of recovering from exception e, rethrow it wrapped
+     *  in a generic RuntimeException so it is not caught by the
+     *  rule function catches.  Exception e is the "cause" of the
+     *  RuntimeException.
+     */
+	 
+    @Override
+    public void recover(Parser recognizer, RecognitionException e) {
+        throw e;
+    }
+
+    /** Make sure we don't attempt to recover from problems in subrules. */
+    @Override
+    public void sync(Parser recognizer) { }
+}
