b94a31a89e0e9c116337ae1a595fe6f183543b42
chielpeters <chielio007@gmail.com>
2014-02-27 16:13:45 +0100

    fixed imports

--- QLNEW/src/ql_obj_alg/operation/typechecker/ITypeCheck.java
+++ QLNEW/src/ql_obj_alg/operation/typechecker/ITypeCheck.java
@@ -1,6 +1,6 @@
 package ql_obj_alg.operation.typechecker;
 
-import ql_obj_alg.errors.error_reporting.ErrorReporting;
+import ql_obj_alg.report_system.error_reporting.ErrorReporting;
 import ql_obj_alg.types.TypeEnvironment;
 
 public interface ITypeCheck {
