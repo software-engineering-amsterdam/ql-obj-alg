ed15067a2c4183b8c37967c8aa7397bcbd323e01
Mary Gouseti <mgouseti@gmail.com>
2014-02-27 17:00:26 +0100

    Concrete class of TNumber

--- QLNEW/src/ql_obj_alg/types/TNumber.java
+++ QLNEW/src/ql_obj_alg/types/TNumber.java
@@ -1,6 +1,6 @@
 package ql_obj_alg.types;
 
-public abstract class TNumber extends Type {
+public class TNumber extends Type {
 
 	@Override
 	public boolean isComparable(Type t) {
@@ -30,5 +30,35 @@ public abstract class TNumber extends Type {
 	@Override
 	public boolean isDate() {
 		return false;
+	}
+
+	@Override
+	public Type merge(Type t) {
+		return t.merge(this);
+	}
+
+	@Override
+	public Type merge(TBoolean t) {
+		return new TUniversal();
+	}
+
+	@Override
+	public Type merge(TInteger t) {
+		return t;
+	}
+
+	@Override
+	public Type merge(TString t) {
+		return new TUniversal();
+	}
+
+	@Override
+	public Type merge(TUniversal t) {
+		return t;
+	}
+
+	@Override
+	public String toString() {
+		return "numeric";
 	}
 }
