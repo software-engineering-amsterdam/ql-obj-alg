57d8f33ebe0b72a23d111a8defb62e1860e27eb4
Mary Gouseti <mgouseti@gmail.com>
2014-03-05 12:51:33 +0100

    Commented out broken tests

--- QLNEW/src/ql_obj_alg/unit_tests/operations/CyclicDependenciesTest.java
+++ QLNEW/src/ql_obj_alg/unit_tests/operations/CyclicDependenciesTest.java
@@ -2,17 +2,25 @@ package ql_obj_alg.unit_tests.operations;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import ql_obj_alg.object_algebra_interfaces.IFormAlg;
+import ql_obj_alg.object_algebra_interfaces.IStmtAlg;
 import ql_obj_alg.operation.builder.IBuildF;
 import ql_obj_alg.operation.cyclic_dependencies.FormDependencies;
+import ql_obj_alg.operation.cyclic_dependencies.modules.Cycle;
 import ql_obj_alg.parsers.antlr4_generated_parser.QLParser;
 import ql_obj_alg.parsers.parser.Parser;
 import ql_obj_alg.report_system.error_reporting.ErrorReporting;
+import ql_obj_alg.report_system.errors.CyclicDependencyError;
+import ql_obj_alg.types.TBoolean;
+import ql_obj_alg.types.TInteger;
 
 public class CyclicDependenciesTest{
 
@@ -34,172 +42,172 @@ public class CyclicDependenciesTest{
 	@Test
 	public void testValueDependencyCycle() {
 		
-		target = "variable";
-		String input = "form id {\n "+target+": \"label\" integer ("+target+") \n "+target+": \"label\" integer \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-		
-		assertEquals(1,errors.size());
-		
-		message = errors.get(0);
-		expected = "Cyclic dependency: "+target+" -> "+target;
-		
-		assertEquals(expected,message);
-		
+//		target = "question";
+//
+//		
+//		
+//		assertEquals(1,report.numberOfErrors());
+//		
+//		Cycle cycle = new Cycle();
+//		
+//		cycle.add(target);
+//		assertTrue(report.containsError(new CyclicDependencyError(cycle)));
+		fail();
 	}
 	
 	@Test
 	public void testDefinitionDependencyCycle() {
-		String x = "X";
-		String y = "Y";
-		String input = "form id {\n if("+x+"){\n "+y+": \"label1\" boolean\n} if("+y+"){\n "+x+": \"label2\" boolean\n}\n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-
-		assertEquals(errors.size(),1);
-		
-		message = errors.get(0);
-		expected = "Cyclic dependency: "+x+" -> "+y+" -> "+x;
-		String alternative = "Cyclic dependency: "+y+" -> "+x+" -> "+y;
-		assertTrue(expected.equals(message) || alternative.equals(message));		
+//		String x = "X";
+//		String y = "Y";
+//		String input = "form id {\n if("+x+"){\n "+y+": \"label1\" boolean\n} if("+y+"){\n "+x+": \"label2\" boolean\n}\n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fd).dependencies();
+//		List<String> errors = report.getErrors();
+//
+//		assertEquals(errors.size(),1);
+//		
+//		message = errors.get(0);
+//		expected = "Cyclic dependency: "+x+" -> "+y+" -> "+x;
+//		String alternative = "Cyclic dependency: "+y+" -> "+x+" -> "+y;
+//		assertTrue(expected.equals(message) || alternative.equals(message));	
+	fail();
 	}
 	
 	@Test
 	public void testNestedDependencyCycle() {
-		String x1 = "X1";
-		String x2 = "X2";
-		String x3 = "X3";
-		
-		String input = "form id {\n "
-				+ "if("+x1+"){\n"
-					+ x2+": \"label2\" boolean\n"
-					+ "if("+x2+"){\n"
-						+ x3+": \"label1\" boolean "
-					+ "}\n"
-				+"}"
-				+ "if("+x3+"){\n "
-					+x1+": \"label3\" boolean"
-				+ "\n}"
-				+ "\n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-		
-		assertEquals(2,errors.size());
-		
-		message = errors.get(0);
-		expected =  "Cyclic dependency: "+x1+" -> "+x3+" -> "+x1;
-		
-		assertEquals(expected,message);
-		
-		message = errors.get(1);
-		expected =  "Cyclic dependency: "+x1+" -> "+x3+" -> "+x2+" -> "+x1;
-		
-		assertEquals(expected,message);
+//		String x1 = "X1";
+//		String x2 = "X2";
+//		String x3 = "X3";
+//		
+//		String input = "form id {\n "
+//				+ "if("+x1+"){\n"
+//					+ x2+": \"label2\" boolean\n"
+//					+ "if("+x2+"){\n"
+//						+ x3+": \"label1\" boolean "
+//					+ "}\n"
+//				+"}"
+//				+ "if("+x3+"){\n "
+//					+x1+": \"label3\" boolean"
+//				+ "\n}"
+//				+ "\n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fd).dependencies();
+//		List<String> errors = report.getErrors();
+//		
+//		assertEquals(2,errors.size());
+//		
+//		message = errors.get(0);
+//		expected =  "Cyclic dependency: "+x1+" -> "+x3+" -> "+x1;
+//		
+//		assertEquals(expected,message);
+//		
+//		message = errors.get(1);
+//		expected =  "Cyclic dependency: "+x1+" -> "+x3+" -> "+x2+" -> "+x1;
+//		
+//		assertEquals(expected,message);
+		fail();
 	}
 	
 	@Test
 	public void testBothDependenciesCycle() {
-		String x1 = "X1";
-		String x2 = "X2";
-		String x3 = "X3";
-		String x4 = "X4";
-		String input = "form id {\n "
-				+ "if("+x1+"){\n"
-					+ x2+": \"label2\" boolean ("+x3+")\n"
-					+ "if("+x2+"){\n"
-						+ x3+": \"label1\" boolean "
-					+ "}\n"
-				+"}"
-				+ "if("+x3+"){\n "
-					+x4+": \"label3\" boolean"
-				+ "\n}"
-				+ x1 +": \"label 4\" integer ("+x4+")"
-				+ "\n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-
-		assertEquals(3,errors.size());
-		
-		//TODO
+//		String x1 = "X1";
+//		String x2 = "X2";
+//		String x3 = "X3";
+//		String x4 = "X4";
+//		String input = "form id {\n "
+//				+ "if("+x1+"){\n"
+//					+ x2+": \"label2\" boolean ("+x3+")\n"
+//					+ "if("+x2+"){\n"
+//						+ x3+": \"label1\" boolean "
+//					+ "}\n"
+//				+"}"
+//				+ "if("+x3+"){\n "
+//					+x4+": \"label3\" boolean"
+//				+ "\n}"
+//				+ x1 +": \"label 4\" integer ("+x4+")"
+//				+ "\n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fd).dependencies();
+//		List<String> errors = report.getErrors();
+//
+//		assertEquals(3,errors.size());
+		fail();
 	}
+	
+
 	
 	@Test
 	public void testDefinitionDependencyNoCycle() {
-		String x = "X";
-		String y = "Y";
-		String input = "form id {\n if("+x+"){\n "+y+": \"label1\" boolean\n} if("+y+"){\n "+x+": \"label2\" boolean\n}"+x+": \"label2\" boolean\n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-
-		assertTrue(errors.isEmpty());
+//		String x = "X";
+//		String y = "Y";
+//		String input = "form id {\n if("+x+"){\n "+y+": \"label1\" boolean\n} if("+y+"){\n "+x+": \"label2\" boolean\n}"+x+": \"label2\" boolean\n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fd).dependencies();
+//		List<String> errors = report.getErrors();
+//
+//		assertTrue(errors.isEmpty());
+		fail();
 	}
 	
 	@Test
 	public void testValueDependencyNoCycle() {
-		target = "variable";
-		String input = "form id {\n "+target+": \"label\" integer (undefined) \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-		
-		assertTrue(errors.isEmpty());
+//		target = "variable";
+//		String input = "form id {\n "+target+": \"label\" integer (undefined) \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fd).dependencies();
+//		List<String> errors = report.getErrors();
+//		
+//		assertTrue(errors.isEmpty());
+		fail();
 	}
 	
 	@Test
 	public void testIndependent(){
-		String x1 = "X1";
-		String x2 = "X2";
-		String x3 = "X3";
-		String x4 = "X4";
-		String input = "form id {\n "
-				+ "if("+x1+"){\n"
-					+ x2+": \"label2\" boolean ("+x1+")\n"
-					+ "if("+x2+"){\n"
-						+ x3+": \"label1\" boolean "
-					+ "}\n"
-				+"}"
-				+ "if("+x3+"){\n "
-					+x4+": \"label3\" boolean"
-				+ "\n}"
-				+ "\n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fd).dependencies();
-		List<String> errors = report.getErrors();
-		
-		assertTrue(errors.isEmpty());
+//		String x1 = "X1";
+//		String x2 = "X2";
+//		String x3 = "X3";
+//		String x4 = "X4";
+//		String input = "form id {\n "
+//				+ "if("+x1+"){\n"
+//					+ x2+": \"label2\" boolean ("+x1+")\n"
+//					+ "if("+x2+"){\n"
+//						+ x3+": \"label1\" boolean "
+//					+ "}\n"
+//				+"}"
+//				+ "if("+x3+"){\n "
+//					+x4+": \"label3\" boolean"
+//				+ "\n}"
+//				+ "\n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fd).dependencies();
+//		List<String> errors = report.getErrors();
+//		
+//		assertTrue(errors.isEmpty());
+		fail();
 	}
 
 }
--- QLNEW/src/ql_obj_alg/unit_tests/operations/TypeCheckerTest.java
+++ QLNEW/src/ql_obj_alg/unit_tests/operations/TypeCheckerTest.java
@@ -35,179 +35,186 @@ public class TypeCheckerTest {
 
 	@Test
 	public void testDuplicateLabels() {
-		
-		String label = "\"Duplicate Label\"";
-		String input = "form id {\n id1: "+label+" integer\n id2: "+label+" boolean \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-		
-		assertEquals(0, report.getErrors().size());
-		
-		List<String> warnings = report.getWarnings();
-		
-		assertEquals(1,warnings.size());
-		
-		message = warnings.get(0);
-		expected = "Duplicate label: "+label;
-		assertEquals(expected,message);
-		
+//		
+//		String label = "\"Duplicate Label\"";
+//		String input = "form id {\n id1: "+label+" integer\n id2: "+label+" boolean \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//		
+//		assertEquals(0, report.getErrors().size());
+//		
+//		List<String> warnings = report.getWarnings();
+//		
+//		assertEquals(1,warnings.size());
+//		
+//		message = warnings.get(0);
+//		expected = "Duplicate label: "+label;
+//		assertEquals(expected,message);
+		fail();
 	}
 	
 	@Test
 	public void testAcceptableDuplicateDefinitions() {
 		
-		String input = "form id {\n id1: \"label1\" integer\n id1: \"label2\" integer \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-		
-		assertEquals(0, report.getErrors().size());
-		assertEquals(0, report.getWarnings().size());		
+//		String input = "form id {\n id1: \"label1\" integer\n id1: \"label2\" integer \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//		
+//		assertEquals(0, report.getErrors().size());
+//		assertEquals(0, report.getWarnings().size());	
+		fail();
 	}
 	
 	@Test
 	public void testUnAcceptableDuplicateDefinitions() {
 		
-		String duplicate = "id1";
-		String type1 = "integer";
-		String type2 = "string";
-		String input = "form id {\n "+duplicate+": \"label1\" "+type1+"\n "+duplicate+": \"label2\" "+type2+" \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-	
-		assertEquals(0, report.numberOfWarning());
-		assertEquals(1, report.numberOErrors());
-		
-		assertEquals(0, report.getWarnings().size());	
-		
-		List<String> errors = report.getErrors();
-		
-		assertEquals(1,errors.size());
-		
-		assertTrue(report.hasError(new DuplicateDeflError()));
-		
-		message = errors.get(0);
-		expected = "Conflicting types of question "+duplicate+", initially declared "+type1+", redeclared "+type2+".";
-		assertEquals(expected,message);
+//		String duplicate = "id1";
+//		String type1 = "integer";
+//		String type2 = "string";
+//		String input = "form id {\n "+duplicate+": \"label1\" "+type1+"\n "+duplicate+": \"label2\" "+type2+" \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//	
+//		assertEquals(0, report.numberOfWarning());
+//		assertEquals(1, report.numberOErrors());
+//		
+//		assertEquals(0, report.getWarnings().size());	
+//		
+//		List<String> errors = report.getErrors();
+//		
+//		assertEquals(1,errors.size());
+//		
+//		assertTrue(report.hasError(new DuplicateDeflError()));
+//		
+//		message = errors.get(0);
+//		expected = "Conflicting types of question "+duplicate+", initially declared "+type1+", redeclared "+type2+".";
+//		assertEquals(expected,message);
+		fail();
 	}
 	
 	@Test
 	public void testUndefinedVariable() {
-		String undefined = "id1";
-		String input = "form id {\n id2: \"label2\" integer ("+undefined+"+3) \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-		
-		assertEquals(0, report.getWarnings().size());
-		
-		List<String> errors = report.getErrors();
-		
-		assertEquals(1,errors.size());
-		
-		message = errors.get(0);
-		expected = "Variable "+undefined+" is undefined.";
-		assertEquals(expected,message);
+//		String undefined = "id1";
+//		String input = "form id {\n id2: \"label2\" integer ("+undefined+"+3) \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//		
+//		assertEquals(0, report.getWarnings().size());
+//		
+//		List<String> errors = report.getErrors();
+//		
+//		assertEquals(1,errors.size());
+//		
+//		message = errors.get(0);
+//		expected = "Variable "+undefined+" is undefined.";
+//		assertEquals(expected,message);
+		fail();
 	}
 	
 	@Test
 	public void testWrongTypeInAdd() {
-		String type = "boolean";
-		String input = "form id {\n id1: \"label1\" "+type+"\n id2: \"label2\" integer (id1+3) \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-		
-		assertEquals(0, report.getWarnings().size());
-		
-		List<String> errors = report.getErrors();
-		
-		assertEquals(1,errors.size());
-		
-		message = errors.get(0);
-		expected = "Wrong type in + expression, expected numeric types, got "+type+" + integer.";
-		assertEquals(expected,message);
+//		String type = "boolean";
+//		String input = "form id {\n id1: \"label1\" "+type+"\n id2: \"label2\" integer (id1+3) \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//		
+//		assertEquals(0, report.getWarnings().size());
+//		
+//		List<String> errors = report.getErrors();
+//		
+//		assertEquals(1,errors.size());
+//		
+//		message = errors.get(0);
+//		expected = "Wrong type in + expression, expected numeric types, got "+type+" + integer.";
+//		assertEquals(expected,message);
+		fail();
 	}
 	
 	@Test
 	public void testIncompatibleTypeInEquals() {
-		String id1 = "id1";
-		String input = "form id {\n "+id1+" :  \"label1\" boolean (3 == true)\n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-		
-		assertEquals(0, report.getWarnings().size());
-		
-		List<String> errors = report.getErrors();
-		
-		assertEquals(1,errors.size());
-		
-		message = errors.get(0);
-		expected = "Incompatible types in == expression, got integer == boolean.";
-		assertEquals(expected,message);
+//		String id1 = "id1";
+//		String input = "form id {\n "+id1+" :  \"label1\" boolean (3 == true)\n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//		
+//		assertEquals(0, report.getWarnings().size());
+//		
+//		List<String> errors = report.getErrors();
+//		
+//		assertEquals(1,errors.size());
+//		
+//		message = errors.get(0);
+//		expected = "Incompatible types in == expression, got integer == boolean.";
+//		assertEquals(expected,message);
+		fail();
 	}
 	
 	@Test
 	public void testWrongTypeInCondition() {
-		String type = "integer";
-		String input = "form id {\n id1: \"label1\" "+type+"\n if(id1){ id2: \"label2\" boolean} \n}";
-
-    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
-    	
-    	IBuildF form = qlParser.forms().frm;
-    	
-		form.build(fcd).collect();
-
-		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
-		form.build(ftc).check();
-		
-		assertEquals(0, report.getWarnings().size());
-		
-		List<String> errors = report.getErrors();
-		
-		assertEquals(1,errors.size());
-		
-		message = errors.get(0);
-		expected = "Wrong type in if-then condition, expected boolean, got "+type+".";
-		assertEquals(expected,message);
+//		String type = "integer";
+//		String input = "form id {\n id1: \"label1\" "+type+"\n if(id1){ id2: \"label2\" boolean} \n}";
+//
+//    	QLParser qlParser = Parser.parse(Parser.getInputStream(input));
+//    	
+//    	IBuildF form = qlParser.forms().frm;
+//    	
+//		form.build(fcd).collect();
+//
+//		ftc = new FormTypeChecker(fcd.getTypeEnvironment(), report);
+//		form.build(ftc).check();
+//		
+//		assertEquals(0, report.getWarnings().size());
+//		
+//		List<String> errors = report.getErrors();
+//		
+//		assertEquals(1,errors.size());
+//		
+//		message = errors.get(0);
+//		expected = "Wrong type in if-then condition, expected boolean, got "+type+".";
+//		assertEquals(expected,message);
+		fail();
 	}
+	
 }
