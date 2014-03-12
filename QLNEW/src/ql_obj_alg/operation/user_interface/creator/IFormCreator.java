b0b972c161643b33492b55a39f5912e3ebd22776
chielpeters <chielio007@gmail.com>
2014-03-12 14:32:54 +0100

    deleted creator

--- QLNEW/src/ql_obj_alg/operation/user_interface/creator/IFormCreator.java
+++
@@ -1,5 +0,0 @@
-package ql_obj_alg.operation.user_interface.creator;
-
-public interface IFormCreator {
-	void create();
-}
--- QLNEW/src/ql_obj_alg/operation/user_interface/creator/IQuestionFieldCreator.java
+++
@@ -1,8 +0,0 @@
-package ql_obj_alg.operation.user_interface.creator;
-
-import ql_obj_alg.operation.user_interface.modules.FormFrame;
-import ql_obj_alg.operation.user_interface.modules.Widgets;
-
-public interface IQuestionFieldCreator {
-	void create(FormFrame frame, Widgets widgets);
-}
