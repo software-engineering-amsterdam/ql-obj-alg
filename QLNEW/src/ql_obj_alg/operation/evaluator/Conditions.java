93fd13d0cb868690ea54470a9cdd016fa67afafe
chielpeters <chielio007@gmail.com>
2014-03-13 15:40:21 +0100

    conditions wrapper

---
+++ QLNEW/src/ql_obj_alg/operation/evaluator/Conditions.java
@@ -0,0 +1,37 @@
+package ql_obj_alg.operation.evaluator;
+
+import java.util.Stack;
+
+public class Conditions {
+
+	Stack<IDepsAndEvalE> conditions;
+	
+	public Conditions(){
+		conditions = new Stack<IDepsAndEvalE>();
+	}
+	
+	public Conditions(Stack<IDepsAndEvalE> conds){
+		this.conditions = conds;
+	}
+	
+	public void addConditional(IDepsAndEvalE cond){
+		conditions.push(cond);
+	}
+	
+	public void removeConditional(){
+		conditions.pop();
+	}
+	
+	public Conditions clone(){
+		return new Conditions((Stack<IDepsAndEvalE>)conditions.clone());
+	}
+
+	public boolean compute(ValueEnvironment valEnv) {
+		for(IDepsAndEvalE cond : conditions){
+			if(!cond.eval(valEnv).getBoolean())
+				return false;
+		}
+		return true;
+	}
+	
+}
