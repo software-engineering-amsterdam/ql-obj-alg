8e8230677ac89e3f5d2c045953b05762ab214fdc
chielpeters <chielio007@gmail.com>
2014-03-13 14:04:58 +0100

    removed eval tests 2

--- QLNEW/src/ql_obj_alg/unit_tests/AllTests.java
+++ QLNEW/src/ql_obj_alg/unit_tests/AllTests.java
@@ -4,12 +4,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import ql_obj_alg.unit_tests.Tree.*;
-import ql_obj_alg.unit_tests.eval.*;
-import ql_obj_alg.unit_tests.operations.*;
+import ql_obj_alg.unit_tests.Tree.exprTests;
+import ql_obj_alg.unit_tests.Tree.frmTests;
+import ql_obj_alg.unit_tests.Tree.stmtTests;
+import ql_obj_alg.unit_tests.operations.CyclicDependenciesTest;
+import ql_obj_alg.unit_tests.operations.DeclarationCollectionTest;
+import ql_obj_alg.unit_tests.operations.TypeCheckerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({exprTests.class,stmtTests.class,frmTests.class,CyclicDependenciesTest.class,DeclarationCollectionTest.class,TypeCheckerTest.class, ExprEvalTests.class})
+@SuiteClasses({exprTests.class,stmtTests.class,frmTests.class,CyclicDependenciesTest.class,DeclarationCollectionTest.class,TypeCheckerTest.class})
 public class AllTests {
 	
 
