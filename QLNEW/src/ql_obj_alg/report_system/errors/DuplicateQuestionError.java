a0d6d5f72614f815b87134d85f6193afe713379d
Mary Gouseti <mgouseti@gmail.com>
2014-03-13 15:07:43 +0100

    DiplicateDeclaration Error Type

--- QLNEW/src/ql_obj_alg/report_system/errors/DuplicateQuestionError.java
+++ QLNEW/src/ql_obj_alg/report_system/errors/DuplicateQuestionError.java
@@ -1,20 +1,15 @@
 package ql_obj_alg.report_system.errors;
 
-import ql_obj_alg.types.Type;
 
 public class DuplicateQuestionError extends GenError {
 	String variable;
-	Type original;
-	Type redeclared;
 	
-	public DuplicateQuestionError(String variable, Type original, Type redeclared){
+	public DuplicateQuestionError(String variable){
 		this.variable = variable;
-		this.original = original;
-		this.redeclared = redeclared;
 	}
 	
 	public String toString(){
-		return "Duplicate declaration of question "+variable+", originally defined "+original.toString()+", redeclared"+redeclared.toString()+".";
+		return "Duplicate declaration of question "+variable+".";
 	}
 	
 	public boolean equals(Object obj){
@@ -24,14 +19,8 @@ public class DuplicateQuestionError extends GenError {
 		if(obj instanceof DuplicateQuestionError){
 			DuplicateQuestionError error = (DuplicateQuestionError) obj;
 			
-			if(this.original == error.original || (this.original != null && this.original.equals(error.original))){
-				if(this.redeclared == error.redeclared || (this.redeclared != null && this.redeclared.equals(error.redeclared))){
-					if(this.variable == error.variable || (this.variable != null && this.variable.equals(error.variable)))
-						return true;
-				}
-			}
-			
-			return false;	
+			if(this.variable == error.variable || (this.variable != null && this.variable.equals(error.variable)))
+				return true;
 		}
 		return false;
 	}
