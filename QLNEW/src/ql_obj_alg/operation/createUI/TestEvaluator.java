5baffbe1f83721c7e5cc972ec7869981644e9dbe
Mary Gouseti <mgouseti@gmail.com>
2014-03-13 13:11:50 +0100

    Trying to use typechecker

--- QLNEW/src/ql_obj_alg/operation/createUI/TestEvaluator.java
+++ QLNEW/src/ql_obj_alg/operation/createUI/TestEvaluator.java
@@ -10,14 +10,26 @@ import ql_obj_alg.operation.builder.IBuildF;
 import ql_obj_alg.operation.evaluator.ExprEvaluator;
 import ql_obj_alg.operation.evaluator.IDepsAndEvalE;
 import ql_obj_alg.operation.evaluator.ValueEnvironment;
+import ql_obj_alg.operation.typechecker.question_type_collection.FormCollectQuestionTypes;
+import ql_obj_alg.operation.typechecker.question_type_collection.ICollect;
+import ql_obj_alg.operation.typechecker.question_type_collection.StmtCollectQuestionTypes;
 import ql_obj_alg.parsers.antlr4_generated_parser.QLParser;
 import ql_obj_alg.parsers.parser.Parser;
+import ql_obj_alg.report_system.error_reporting.ErrorReporting;
+import ql_obj_alg.types.TypeEnvironment;
 public class TestEvaluator {
 
 	public static void main(String[] args) {
 		try {
 			QLParser qlParser = Parser.parse(Parser.getInputStream(new FileInputStream(args[0])));
 	    	IBuildF form = qlParser.form().frm;
+	    	
+	    	ErrorReporting report = new ErrorReporting();
+	    	TypeEnvironment typeEnv = new TypeEnvironment();
+	    	
+//	    	IStmtAlg<Void, ICollect> collectQuestions = new StmtCollectQuestionTypes();
+//	    	IFormAlg<Void,ICollect,ICollect> collectForm = new FormCollectQuestionTypes();
+//	    	form.build(Void,collectQuestions,collectForm).collect(typeEnv);
 	    	
 	    	IExpAlg<IDepsAndEvalE> expAlg = new ExprEvaluator();
 	    	IStmtAlg<IDepsAndEvalE,ICreate> stmtAlg = new StmtUI();
