e1a4647c58a369e0c3bf2d5dd95dde1d70f46d64
Mary Gouseti <mgouseti@gmail.com>
2014-03-13 10:37:42 +0100

    Extracted values from value environment

--- QLNEW/src/ql_obj_alg/operation/evaluator/IDepsAndEvalE.java
+++ QLNEW/src/ql_obj_alg/operation/evaluator/IDepsAndEvalE.java
@@ -3,8 +3,9 @@ package ql_obj_alg.operation.evaluator;
 import java.util.List;
 
 import ql_obj_alg.operation.evaluator.value.Value;
+import ql_obj_alg.user_interface.modules.FormFrame;
 
 public interface IDepsAndEvalE {
-	Value eval(ValueEnvironment valEnv);
+	Value eval(FormFrame frame);
 	List<String> deps();
 }
--- QLNEW/src/ql_obj_alg/operation/evaluator/ValueEnvironment.java
+++ QLNEW/src/ql_obj_alg/operation/evaluator/ValueEnvironment.java
@@ -10,7 +10,7 @@ import ql_obj_alg.user_interface.widgets.ObservableWidget;
 public class ValueEnvironment{
 
 	private Map<String, ObservableWidget> registry = new HashMap<String,ObservableWidget>();
-	private Map<String,Value> questionsMap = new HashMap<String,Value>();
+//	private Map<String,Value> questionsMap = new HashMap<String,Value>();
 	
 	public void initObservable(String name) {
 		if (!registry.containsKey(name)) {
@@ -24,28 +24,18 @@ public class ValueEnvironment{
 		return registry.get(name);
 	}
 
-	private void initQuestion(String varName) {
-		if(!questionsMap.containsKey(varName)){
-			questionsMap.put(varName, new VUndefined());
-		}
-	}	
+//	private void initQuestion(String varName) {
+//		if(!questionsMap.containsKey(varName)){
+//			questionsMap.put(varName, new VUndefined());
+//		}
+//	}	
 	
-	public Value getQuestionValue(String varName){
-		initQuestion(varName);
-		return questionsMap.get(varName);
-	}
+//	public Value getQuestionValue(String varName){
+//		initQuestion(varName);
+//		return questionsMap.get(varName);
+//	}
 
-	public void setQuestionValue(String varName, Value v){
-		questionsMap.put(varName,v);		
-	}
-	
-	public String toJsonString(){
-		String ret = "{ \n";
-		for(String key : questionsMap.keySet()){
-			ret += "\"" + key + "\"" + ":" + "\"" + questionsMap.get(key) + "\",\n";
-		}
-		ret += "}";
-		return ret;
-	}
-
+//	public void setQuestionValue(String varName, Value v){
+//		questionsMap.put(varName,v);		
+//	}
 }
