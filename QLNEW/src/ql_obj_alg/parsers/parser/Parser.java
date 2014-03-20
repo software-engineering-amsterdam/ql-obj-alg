f3ee6218e5ef71eb5a5e4c330a65ccad11cd2549
chielpeters <chielio007@gmail.com>
2014-03-20 10:26:18 +0100

    removed ANTLR dependencies in Parser

--- QLNEW/src/ql_obj_alg/parsers/parser/Parser.java
+++ QLNEW/src/ql_obj_alg/parsers/parser/Parser.java
@@ -1,83 +1,92 @@
 package ql_obj_alg.parsers.parser;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringWriter;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import ql_obj_alg.object_algebra_interfaces.IExpAlg;
-import ql_obj_alg.object_algebra_interfaces.IFormAlg;
-import ql_obj_alg.object_algebra_interfaces.IStmtAlg;
-import ql_obj_alg.operation.noop.ExprNoop;
-import ql_obj_alg.operation.noop.INoop;
-import ql_obj_alg.operation.printer.ExprPrecedence;
-import ql_obj_alg.operation.printer.FormFormat;
-import ql_obj_alg.operation.printer.boxalg.IFormat;
-import ql_obj_alg.operation.typechecker.question_type_collection.FormCollectQuestionTypes;
-import ql_obj_alg.operation.typechecker.question_type_collection.ICollect;
-import ql_obj_alg.operation.typechecker.question_type_collection.StmtCollectQuestionTypes;
 import ql_obj_alg.parsers.antlr4_generated_parser.Builder;
 import ql_obj_alg.parsers.antlr4_generated_parser.QLLexer;
 import ql_obj_alg.parsers.antlr4_generated_parser.QLParser;
-import ql_obj_alg.report_system.error_reporting.ErrorReporting;
-import ql_obj_alg.types.TypeEnvironment;
 
 public class Parser {
-    public static void main(String[] args) throws Exception {
-    	QLParser qlParser = parse(getInputStream(new FileInputStream(args[0])));
-    	Builder form = (Builder) qlParser.form().frm;
-    	typeCheckerForm(form);
-    	printForm(form);
-    }
 
-	private static void printForm(Builder form) {
-		
-		FormFormat fFormat = new FormFormat(new ExprPrecedence());
-		StringWriter w = new StringWriter();
-		IFormat printingForm = (IFormat) form.build(fFormat,fFormat,fFormat);
-		printingForm.format(0, false, w);
-        System.out.println(w);
+	
+	public static Builder getExpressions(FileInputStream fis){
+		ANTLRInputStream inputStream;
+		try {
+			inputStream = new ANTLRInputStream(fis);
+			return getExpressions(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assert false: "file not found error";
+		return null;
 	}
-       
-    public static QLParser parse(ANTLRInputStream input){
+	
+	public static Builder getExpressions(String input){
+		ANTLRInputStream inputStream = new ANTLRInputStream(input);
+		return getExpressions(inputStream);
+	}
+
+    
+	private static Builder getExpressions(ANTLRInputStream inputStream) {
+		QLParser qlParser = parse(inputStream);
+		return (Builder) qlParser.expr().exp;
+	}
+
+	public static Builder getStatements(FileInputStream fis){
+		ANTLRInputStream inputStream;
+		try {
+			inputStream = new ANTLRInputStream(fis);
+			return getStatements(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assert false: "file not found error";
+		return null;
+	}
+	
+	public static Builder getStatements(String input){
+		ANTLRInputStream inputStream = new ANTLRInputStream(input);
+		return getStatements(inputStream);
+	}
+
+    
+	private static Builder getStatements(ANTLRInputStream inputStream) {
+		QLParser qlParser = parse(inputStream);
+		return (Builder) qlParser.stat().stmt;
+	}	
+	
+
+	public static Builder getForm(String input){
+		ANTLRInputStream inputStream = new ANTLRInputStream(input);
+		return getForm(inputStream);
+	}
+	
+	public static Builder getForm(FileInputStream fis){
+		ANTLRInputStream inputStream;
+		try {
+			inputStream = new ANTLRInputStream(fis);
+			return getForm(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assert false: "file not found error";
+		return null;
+	}	
+	
+	
+    private static Builder getForm(ANTLRInputStream inputStream) {
+		QLParser qlParser = parse(inputStream);
+		return (Builder) qlParser.form().frm;
+	}
+    
+	private static QLParser parse(ANTLRInputStream input){
         QLLexer lexer = new QLLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         return new QLParser(tokens);      	
     }
-    
-    public static ANTLRInputStream getInputStream(String QL){
-    	return new ANTLRInputStream(QL);
-    }
-
-    public static ANTLRInputStream getInputStream(FileInputStream fis) throws FileNotFoundException, IOException{
-    	return new ANTLRInputStream(fis);
-    }
-    
-	public static boolean typeCheckerForm(Builder form) {
-		TypeEnvironment typeEnv = new TypeEnvironment();
-		ErrorReporting report = new ErrorReporting();
-		
-		IFormAlg<INoop,ICollect,ICollect> collectForm = new FormCollectQuestionTypes();
-		IStmtAlg<INoop,ICollect> collectStmt = new StmtCollectQuestionTypes();
-		IExpAlg<INoop> exprAlg = new ExprNoop();
-		ICollect collectTypes = (ICollect) form.build(collectForm,collectStmt,exprAlg);
-		collectTypes.collect(typeEnv,report);
-		
-//		IFormAlg<IExpType,ITypeCheck,ITypeCheck> typeCheckForm = new FormTypeChecker();
-//		ITypeCheck checkTypes = (ITypeCheck) form.build(typeCheckForm);
-//		checkTypes.check(typeEnv, report);
-//		
-//		IFormAlg<IExpDependency,IDependencyGraph,IDependencyGraph> formDependencies = new FormDependencies(report);
-//		IDependencyGraph cyclesDetection = (IDependencyGraph) form.build(formDependencies);
-//		cyclesDetection.dependencies(new FillDependencyGraph());
-//
-//		report.printErrors();
-//		report.printWarnings();
-		
-		return report.numberOfErrors() == 0;
-	}
 
 }
